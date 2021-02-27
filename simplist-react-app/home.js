66c885f989da85876478dbe64f1ff24974a417e3
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-02-26 23:22:29 -0800

    simplist app complete

--- simplist-react-app/home.js
+++ simplist-react-app/home.js
@@ -1,17 +1,4 @@
-'use strict'
-
-const e = React.createElement
-
-class Home extends React.Component {
-
-  render() {
-
-    return (
-      'Welcome home.'
-    )
-
-  }
-}
-
-const domContainer = document.querySelector('#home_container')
-ReactDOM.render(e(Home), domContainer)
+ReactDOM.render(
+  React.createElement('div', null, 'Welcome home.'),
+  document.querySelector('#home_container')
+)
