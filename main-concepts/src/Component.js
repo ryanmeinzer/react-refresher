d3455200f01f1f7c0f872c34188e1775f58520af
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-03-01 20:42:11 -0800

    starting main concepts practice

---
+++ main-concepts/src/Component.js
@@ -0,0 +1,11 @@
+// Function Component
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>
+}
+
+// Class Component
+class Welcome extends React.Component {
+    render() {
+        return <h1>Hello, {this.props.name}</h1>
+    }
+}
