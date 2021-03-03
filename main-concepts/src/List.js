67d331789c3a2937b79de18be92897c824318f13
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-03-02 21:32:04 -0800

    refactored to include full map in jsx

--- main-concepts/src/List.js
+++ main-concepts/src/List.js
@@ -1,10 +1,17 @@
 function List() {
     const numbers = [1, 2, 3, 4, 5]
-    const listItems = numbers.map((number) =>
-        <li key={number.toString()}>{number}</li>
-    )
+    // const listItems = numbers.map((number) =>
+    //     <li key={number.toString()}>{number}</li>
+    // )
+    // return (
+    //     <ul>{listItems}</ul>
+    // )
     return (
-        <ul>{listItems}</ul>
+        <ul>
+            {numbers.map((number) =>
+                <li key={number.toString()}>{number}</li>
+            )}
+        </ul>
     )
 }
 
